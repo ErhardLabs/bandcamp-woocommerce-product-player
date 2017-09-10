8277583bf3223f6ad1aa885e9d4c53a770d9e417
Grayson Erhard <grayson@graphiclux.com>
2017-09-10 13:32:06 -0600

    finished and debugged on live

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function bwpp_scripts() {
 
   wp_register_script('bwpp', WP_PLUGIN_URL.'/bandcamp-woocommerce-product-player/assets/js/bwpp.js', array('jquery'), false, true);
 
-  wp_enqueue_script('slider-div');
+  wp_enqueue_script('bwpp');
 
 }
 
@@ -34,7 +34,7 @@ function woo_add_custom_general_fields() {
       array(
           'id'          => '_text_field',
           'label'       => __( 'Bandcamp Track ID', 'woocommerce' ),
-          'placeholder' => '682966733',
+          'placeholder' => 'track=682966733',
           'desc_tip'    => 'true',
           'description' => __( 'Enter the Bandcamp track ID found in the iframe embed code here.', 'woocommerce' )
       )
@@ -56,60 +56,80 @@ function woo_add_custom_general_fields_save($post_id) {
 }
 
 
+add_action('woocommerce_before_shop_loop_item', 'new_product_image_iframe');
 add_action('woocommerce_single_product_image_thumbnail_html', 'new_product_image_iframe');
 function new_product_image_iframe() {
 
   global $post;
 
-//  <iframe style="border: 0; width: 350px; height: 442px;" src="https://bandcamp.com/EmbeddedPlayer/track=682966733/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://graysonerhard.bandcamp.com/track/karma">Karma by Grayson Erhard</a></iframe>
 
   $bandcampID = get_post_meta($post->ID, '_text_field', true);
 
   if ($bandcampID) {
 
-    ?>
-    <div class="bwpp_iframe_wrap">
-      <iframe style="border: 0; width: 100%; height: 100vh;" src="https://bandcamp.com/EmbeddedPlayer/track=<?php echo $bandcampID; ?>/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><?php echo the_title(); ?></iframe>
-    </div>
-    <?php
+    if (is_single()) {
+      ?>
+      <div class="bwpp_iframe_wrap">
+        <iframe class="bwpp" style="border: 0; width: 100%; height: 100vh;" src="https://bandcamp.com/EmbeddedPlayer/<?php echo $bandcampID; ?>/size=large/bgcol=000/linkcol=e99708/tracklist=false/transparent=true/" seamless></iframe>
+      </div>
+      <?php
+    } else {
+      remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+
+      ?>
+      <div class="bwpp_iframe_wrap">
+        <iframe class="bwpp" style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/<?php echo $bandcampID; ?>/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/" seamless></iframe>
+      </div>
+      <?php
+    }
+
 
   } else {
 
-    // DISPLAY USUAL PRODUCT IMAGE
+    if (is_single()) {
+// DISPLAY USUAL PRODUCT IMAGE
 
-    $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
-    $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-    $full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, 'full' );
-    $image_title       = get_post_field( 'post_excerpt', $post_thumbnail_id );
-    $placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
-    $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
-        'woocommerce-product-gallery',
-        'woocommerce-product-gallery--' . $placeholder,
-        'woocommerce-product-gallery--columns-' . absint( $columns ),
-        'images',
-    ) );
+      $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
+      $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+      $full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, 'full' );
+      $image_title       = get_post_field( 'post_excerpt', $post_thumbnail_id );
+      $placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
+      $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
+          'woocommerce-product-gallery',
+          'woocommerce-product-gallery--' . $placeholder,
+          'woocommerce-product-gallery--columns-' . absint( $columns ),
+          'images',
+      ) );
 
-    $attributes = array(
-        'title'                   => $image_title,
-        'data-src'                => $full_size_image[0],
-        'data-large_image'        => $full_size_image[0],
-        'data-large_image_width'  => $full_size_image[1],
-        'data-large_image_height' => $full_size_image[2],
-    );
+      $attributes = array(
+          'title'                   => $image_title,
+          'data-src'                => $full_size_image[0],
+          'data-large_image'        => $full_size_image[0],
+          'data-large_image_width'  => $full_size_image[1],
+          'data-large_image_height' => $full_size_image[2],
+      );
 
-    if ( has_post_thumbnail() ) {
-      $html  = '<div data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_size_image[0] ) . '">';
-      $html .= get_the_post_thumbnail( $post->ID, 'shop_single', $attributes );
-      $html .= '</a></div>';
+      if ( has_post_thumbnail() ) {
+        $html  = '<div data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_size_image[0] ) . '">';
+        $html .= get_the_post_thumbnail( $post->ID, 'shop_single', $attributes );
+        $html .= '</a></div>';
+      } else {
+        $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
+        $html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+        $html .= '</div>';
+      }
+
+      echo $html;
+
+      do_action( 'woocommerce_product_thumbnails' );
+
     } else {
-      $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
-      $html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-      $html .= '</div>';
+      add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
     }
 
-    echo $html;
 
-    do_action( 'woocommerce_product_thumbnails' );
+
+
 
   }
 
