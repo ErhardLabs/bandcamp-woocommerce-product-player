abce3dc59f04de38dccbd8efbdb9a618ef13d4cc
Grayson Erhard <grayson@graphiclux.com>
2018-05-03 14:21:21 -0600

    js folder addition?

---
+++ assets/js/bwpp.js
@@ -0,0 +1,15 @@
+// $=jQuery;
+//
+// $(document).ready(function() {
+//   // $('#bwpp').load(function(){
+//   //   $('#bwpp').contents().find('#buyordownload').hide();
+//   // });
+//
+//
+//     $("#bwpp").load(function() {
+//       var doc = this.contentDocument || this.contentWindow.document;
+//       var target = doc.getElementById("target");
+//       target.innerHTML = "Found It!";
+//     });
+//
+// });
